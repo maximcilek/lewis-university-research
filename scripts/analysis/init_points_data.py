66a8b51645ee62958628130fe79c78572f82ba5c
Maxim Cilek <maxssportsanalysis@gmail.com>
2026-05-22 12:14:39 -0500

    Updated

--- scripts/analysis/init_points_data.py
+++ scripts/analysis/init_points_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pathlib
 from collections import defaultdict, deque
+from itertools import zip_longest
 import logging
 import sys
 sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent.parent))
@@ -8,6 +9,8 @@ import tennisabstractscraper.models.data_objects as data_objects
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 logger = logging.getLogger(__name__)
+
+TIEBREAK_MATCHES = []
 
 def load_charting_matches(filename):
     charting_matches = data_objects.DataObjectFactory.create(filename).data
@@ -37,6 +40,82 @@ def load_points(filenames):
     logger.info(f"Loaded {len(points)} Points")
     return points_by_match_set_game
 
+# player_game_points_to_int
+def normalize_game_points_score(game_points):
+
+    def normalize_single_points_score(score):
+        score = score.strip().lower()
+        if not isinstance(score, str):
+            logger.fatal(f"Unexpected Player Game Points Value Type, Expected String: {score} ({type(score)})")
+            sys.exit(1)
+        if not score.isdigit():
+            if score == "ad":
+                return 45
+            logger.fatal(f"Unexpected Game Points Value (non-integer and not AD): {score}")
+            sys.exit(1)
+        return int(score)
+
+    game_points = game_points.strip().lower() # Points1-Points2
+    if not isinstance(game_points, str):
+        logger.fatal(f"Unexpected Player Game Points Value Type, Expected String: {game_points} ({type(game_points)})")
+        sys.exit(1)
+    server_score, returner_score = game_points.split("-")
+    return normalize_single_points_score(server_score), normalize_single_points_score(returner_score)
+
+
+def is_tiebreak_game(points):
+    tennis_scores_map = {
+        "0": 0, "15": 15, "30": 30, "40": 40, "AD": 45
+    }
+
+    num_points = len(points) - 1
+    while num_points > 0:
+        server_score, returner_score = normalize_game_points_score(points[num_points]["Pts"])
+        prev_server_score, prev_returner_score = normalize_game_points_score(points[num_points-1]["Pts"])
+
+        diff = abs(abs(server_score - returner_score) - abs(prev_server_score - prev_returner_score))
+        if diff % 5 > 0:
+            if diff != 1:
+                print(f"Prev/Curr (diff: {((diff)%5):>2}): {points[num_points-1]['Pts']:>5} --> {server_score}-{returner_score}")
+                quit()
+            return True
+        num_points -= 1
+    return False
+
+def get_official_score(match):
+    score = match.get("score")
+    match_score = match.get("match_score")
+
+    candidates = []
+
+    if pd.notna(score):
+        candidates.append(("score", str(score).strip()))
+
+    if pd.notna(match_score):
+        candidates.append(("match_score", str(match_score).strip()))
+    
+    if len(candidates) == 2:
+        if score != match_score:
+            set_scores = score.split(" ")
+            match_set_scores = match_score.split(" ")
+            new_official_score_parts = []
+            for set_score, match_set_score in zip_longest(set_scores, match_set_scores):
+                if set_score and match_set_score:
+                    if "(" in match_set_score:
+                        new_official_score_parts.append(match_set_score)
+                    else:
+                        new_official_score_parts.append(set_score)
+                else:
+                    new_official_score_parts.append(set_score or match_set_score)
+            return (" ".join(new_official_score_parts))
+        else:
+            return max(candidates, key=lambda x: (len(x[1].split()), x[0] == "score"), default=(None, None))[1]
+    elif len(candidates) == 1:
+        return candidates[0][1]
+    else:
+        print(f"Failed to find match score candidates: {match}")
+        quit()
+
 if __name__ == "__main__":
     DATA_DIR = pathlib.Path(__file__).resolve().parent.parent.parent / "data"
     raw_points_filenames = [DATA_DIR / "raw/tennisabstract/tennis_MatchChartingProject-master/charting-m-points-to-2009.csv", 
@@ -59,36 +138,27 @@ if __name__ == "__main__":
             tournament_type = tournament_type[:-3]
         match_year = int(match_id[:4])
 
-        if match_id not in points_by_id or (("RET" in str(m["score"]) or "RET" in str(m["match_score"])) or ("DEF" in str(m["score"]) or "DEF" in str(m["match_score"]))): # or (any(keyword in match_id for keyword in IGNORED_TOURNAMENT_TYPES))
+        if match_id not in points_by_id or "Laver_Cup" in match_id or (("RET" in str(m["score"]) or "RET" in str(m["match_score"])) or ("DEF" in str(m["score"]) or "DEF" in str(m["match_score"]))): # or (any(keyword in match_id for keyword in IGNORED_TOURNAMENT_TYPES))
             continue
         # match_points = dict(sorted(points_by_id[match_id].items(), key=lambda x: int(x[0])))
         #print(f"{match_id} has {len(match_points)} Points")
         #print(match_points[1])
         #quit()
 
+        m['official_score'] = get_official_score(m)
+
         match_sets = points_by_id[match_id]
 
-        print(f"\n🎾 Match: {match_id}")
-        print(f"Sets: {len(match_sets)}")
-        print("=" * 80)
+        logger.debug(f"Loading Match: {match_id}")
         for set_number, games in match_sets.items():
-            print(f"\n[SET {set_number}] — {len(games)} games")
-            print("-" * 80)
             for game_number, points in games.items():
-                print(f"  Game {game_number:>2} — {len(points)} points")
-                for point_number, point_data in points.items():
-                    score = point_data["Pts"]
-                    winner = point_data["PtWinner"]
-                    server = point_data["Svr"]
-                    print(
-                        f"    Pt {point_number:>2} | "
-                        f"Score: {score:<7} | "
-                        f"Server: P{server} | "
-                        f"Winner: P{winner}"
-                    )
-                print()
-                break  # To get every game
-        print("=" * 80)
+                is_tiebreak = is_tiebreak_game(points)
+                if is_tiebreak:
+                    logging.info(f"Found tiebreak game: {game_number} ({match_id})")
+                    last_game_number, last_game_points = list(games.items())[-1]
+                    last_point = last_game_points[-1]
+                    print(f"{last_point['Gm1']}-{last_point['Gm2']} | {m['official_score']}")
+                    print(last_point)
+                    quit()
         count_matches += 1
-        quit()
     print(f"Successfully Validated {count_matches} Matches")
